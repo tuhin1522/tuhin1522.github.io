ad63a6d34fd7e4efc3bff75881a93eba98efd583
MD. TUHIN MOLLA <115881424+tuhin1522@users.noreply.github.com>
2026-04-28 20:35:27 +0600

    Update base path for asset resolution
    Changed base path from '/' to './' to ensure relative paths for assets.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  // Changed from '/' to './' to ensure relative paths for assets
+  base: './', 
   plugins: [react()],
 })
